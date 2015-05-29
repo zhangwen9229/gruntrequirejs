1d5cfd197f97fb3c4c9fc06f8046eae545678155
vendy <wenshunzhang@gtriches.com>
2015-05-29 17:15:09 +0800

    修正 Gruntfile.js  针对 requirejs =

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,32 +3,22 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['scripts/*.js'],
-        dest: 'dist/main.js'
-      },
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'dist/main.js',//src/<%= pkg.name %>.js
-        dest: 'scripts/main.min.js'//<%= pkg.name %>.min.js
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "./node_modules",
+          mainConfigFile: "./scripts/main.js",
+          name: "../scripts/main", // assumes a production build using almond
+          out: "./build/main.min.js"
+        }
       }
     }
   });
 
-  // 加载包含 "uglify" 任务的插件。
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  // // 加载包含 "requirejs" 任务的插件。
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   // 默认被执行的任务列表。
-  grunt.registerTask('default', ['concat','uglify']);
+  grunt.registerTask('default', ['requirejs']);
 
 };
